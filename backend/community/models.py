ca5390884d5c15e3074d736d4b18aa1e0fd4cd90
Taegyung Gong <gtg92t@gmail.com>
2020-06-11 17:13:17 +0900

    Modify community model

--- backend/community/models.py
+++ backend/community/models.py
@@ -10,10 +10,24 @@ class BaseModel(models.Model):
         abstract = True
 
 class Movie(models.Model):
-    movie_api_id = models.IntegerField()
+    title = models.CharField(max_length=150)
+    original_title = models.CharField(max_length=150)
+    release_date = models.DateField()
+    popularity = models.FloatField()
+    vote_count = models.IntegerField()
+    vote_average = models.FloatField()
+    adult = models.BooleanField()
+    overview = models.TextField()
+    original_language = models.CharField(max_length=150)
+    poster_path = models.CharField(max_length=150)
+    backdrop_path = models.CharField(max_length=150)
+    genres = models.ManyToManyField("Genre")
     liked_users = models.ManyToManyField(
         settings.AUTH_USER_MODEL, related_name='like_movies')
 
+class Genre(models.Model):
+    name = models.CharField(max_length=150)
+    
 class Review(BaseModel):
     movie = models.ForeignKey(
         Movie, on_delete=models.CASCADE, related_name='reviews')
