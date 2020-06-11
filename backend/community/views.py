1eeb6b09981283c3f755a5f421e92e548453de3a
Taegyung Gong <gtg92t@gmail.com>
2020-06-11 17:38:46 +0900

    Add movie list serializer & view.py

--- backend/community/views.py
+++ backend/community/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import Movie
+from .serializers import MovieListSerializer
+
+
+@api_view(['GET'])
+def movie_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieListSerializer(movies, many=True)
+    return Response(serializer.data)
