de1a0bac6abdf20d0796972fde798d3d872de40e
jenny.kr <jenny.it.kr@gmail.com>
2020-06-11 15:41:57 +0900

    Add login api call

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,3 +1,7 @@
 export default {
-  BASE: "https://api.themoviedb.org/3/movie/",
+  TMDB_BASE: "https://api.themoviedb.org/3/movie/",
+  DB_BASE: "http://localhost:8000/",
+  DB_ROUTES: {
+    login: "accounts/rest-auth/",
+  },
 };
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,26 +2,34 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import API from "../api/api";
+import router from "../router";
 
 import axios from "axios";
+import cookies from "vue-cookies";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    authToken: cookies.get("auth-token"),
     movies: [],
   },
-  getters: {},
+  getters: {
+    isLoggedIn: (state) => !!state.authToken,
+  },
   mutations: {
     SET_MOVIES(state, movies) {
       state.movies = movies;
-      console.log("done");
+    },
+    SET_TOKEN(state, token) {
+      state.authToken = token;
+      cookies.set("auth-token", token);
     },
   },
   actions: {
     fetchMovies({ commit }) {
       axios
-        .get(API.BASE + "popular", {
+        .get(API.TMDB_BASE + "popular", {
           params: {
             api_key: process.env.VUE_APP_TMDB_API_KEY,
           },
@@ -30,6 +38,24 @@ export default new Vuex.Store({
           commit("SET_MOVIES", res.data.results);
         })
         .catch((err) => console.log(err.response));
+    },
+    postAuthData({ commit }, info) {
+      router.push({ name: "Home" });
+      axios
+        .post(API.DB_BASE + info.route, info.data)
+        .then((res) => {
+          commit("SET_TOKEN", res.data.key);
+          router.push({ name: "Home" });
+        })
+        .catch((err) => console.log(err.response));
+    },
+    login({ dispatch }, loginData) {
+      const info = {
+        data: loginData,
+        route: API.DB_ROUTES.login,
+      };
+      console.log("login");
+      dispatch("postAuthData", info);
     },
   },
   modules: {},
