94c07b3d16897b089ae7b3e87b2e020ab744231b
Taegyung Gong <gtg92t@gmail.com>
2020-06-13 14:22:31 +0900

    Fix views.py

--- backend/community/views.py
+++ backend/community/views.py
@@ -38,18 +38,6 @@ class MovieDetail(APIView):
         serializer = MovieDetailSerializer(Movie)
         return Response(serializer.data)
 
-    # def put(self, request, pk, format=None):
-    #     snippet = self.get_object(pk)
-    #     serializer = SnippetSerializer(snippet, data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # def delete(self, request, pk, format=None):
-    #     snippet = self.get_object(pk)
-    #     snippet.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 class ReviewList(APIView):
     
@@ -128,12 +116,6 @@ class ReviewComments(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# class CommentList(APIView):
-    
-#     def get(self, request, format=None):
-#         comments = Comment.objects.all()
-#         serializer = CommentSerializer(comments, many=True)
-#         return Response(serializer.data)
         
 
 class CommentDetail(APIView):
