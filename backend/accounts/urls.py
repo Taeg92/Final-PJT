aa8ab20f0c5cc09b98c849d784190811b5e72427
Taegyung Gong <gtg92t@gmail.com>
2020-06-17 22:50:16 +0900

    Back | Add user info request

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+from . import views
 
 urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('api-jwt-auth/', obtain_jwt_token),
     path('api-jwt-auth/refresh/', refresh_jwt_token),
     path('api-jwt-auth/verify/', verify_jwt_token),
+    re_path(r'^(?P<username>[\w]+)/$', views.UserInfo.as_view(), name='UserInfo'),
 ]
