9317ed4670097b3c43b894b4c16b72897bf2cf89
jenny.kr <jenny.it.kr@gmail.com>
2020-06-19 00:45:57 +0900

    Front | Fix CommentFrom style for non-member

--- frontend/src/components/CommentForm.vue
+++ frontend/src/components/CommentForm.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="comment-form">
-    <label for="content"></label>
-    <input
-      v-model="submitInfo.content"
-      @keypress.enter="submitComment"
-      type="text"
-    />
-    <i
-      class="far fa-paper-plane create-btn"
-      @click="submitComment"
-    ></i>
+    <div class="member" v-if="isLoggedIn">
+      <label for="content"></label>
+      <input v-model="submitInfo.content" @keypress.enter="submitComment" type="text" />
+      <i class="far fa-paper-plane create-btn" @click="submitComment"></i>
+    </div>
+    <div class="non-member" v-else>회원만 댓글을 작성할 수 있습니다 :)</div>
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapState, mapActions } from "vuex";
 
 export default {
   name: "CommentCreate",
@@ -23,21 +19,22 @@ export default {
       submitInfo: {
         moviePK: this.$route.params.moviePK,
         reviewPK: this.$route.params.reviewPK,
-        content: "",
-      },
+        content: ""
+      }
     };
   },
+  computed: { ...mapState(["isLoggedIn"]) },
   methods: {
     ...mapActions(["createComment"]),
     submitComment() {
       this.createComment(this.submitInfo);
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style>
-.comment-form {
+.comment-form .member {
   display: flex;
   width: 100%;
 }
@@ -61,12 +58,23 @@ export default {
   color: #e50a13b3;
   font-size: 10px;
   cursor: pointer;
-  transition: background-color 0.1s ease-in-out,
-    color 0.1s ease-in-out;
+  transition: background-color 0.1s ease-in-out, color 0.1s ease-in-out;
 }
 
 .create-btn:hover {
   background-color: #e50a13b3;
+  color: white;
+}
+
+.comment-form .non-member {
+  width: 100%;
+  height: 25px;
+  background-color: rgb(136, 135, 135);
+  border-radius: 5px;
+  text-align: center;
+  line-height: 25px;
+  font-size: 12px;
+  font-weight: 600;
   color: white;
 }
 </style>
